46caced190232192cba111e7fa1974306e6a1fb0
Erik Olofsen <e.olofsen@weggetjes.nl>
2024-03-26 19:23:00 +0100

    generalized ppwrite

--- examples/clean.sh
+++ examples/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm *.tex *.log *.dvi *.aux *.ps *.pdf toeps.eps PE* *~
+rm *.tex *.log *.dvi *.aux *.ps *.pdf toeps.eps PE* EPS* *~
 
 for F in axis geoms grid legend options picture pst-coil pst-node pst-plot pstricks subplot ticks write ; do
     pushd $F
